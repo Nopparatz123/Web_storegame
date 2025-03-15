e9ed6b6e80f00da3a27d44c238383236ac54ca9a
Nopparatz123 <155348143+Nopparatz123@users.noreply.github.com>
2025-03-15 17:53:31 +0700

    1

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="./global.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <?php require './widgets/modal.php'; ?>
+    <video autoplay loop muted playsinline class="blackgroud-clip">
+        <source src="./assets/video.mp4">
+    </video>
+
+    <div class="container-fluid" style="padding: 15rem 22rem;">
+        <?php require './widgets/navbar.php'; ?>
+        <div class="title text-center" style="margin-bottom: 50px;">
+            <h1 class="text-light fw-bold" style="font-size: 70px;">ยินดีต้อนรับสู่เว็บควยๆ</h1>
+            <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis labor</p>
+        </div>
+        <marquee class="text-light mb-5"><span class="badge bg-danger me-2"><i class="bi bi-megaphone-fill"></i> ประกาศ</span>ยังไม่เสร็จไอ้สัส !!</marquee>
+        <div class="row">
+            <div class="col">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100 rounded-4" src="./assets/img/2.png" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100 rounded-4" src="./assets/img/3.png" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100 rounded-4" src="./assets/img/4.jpg" alt="Third slide">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ widgets/navbar.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top shadow-sm">
+    <div class="container d-flex justify-content-between align-items-center">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+            <img src="https://preview.redd.it/273w5hzk9iq71.png?width=640&crop=smart&auto=webp&s=ff14b4977315ab54829b21b3c1bf26b749b7f3a2"
+                alt="Logo" width="150px" class="ms-2">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse text-center" id="navbarNav">
+            <ul class="navbar-nav mx-auto g-4">
+                <li class="nav-item me-2">
+                    <a href="#" class="nav-link navbar-optin actives">
+                        <i class="bi bi-house-fill"></i> หน้าหลัก
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link navbar-optin">
+                        <i class="bi bi-shop"></i> ร้านค้า
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link navbar-optin">
+                        <i class="bi bi-credit-card"></i> เติมเงิน
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link navbar-optin">
+                        <i class="bi bi-file-earmark-person-fill"></i> ช่องทางการติดต่อ
+                    </a>
+                </li>
+            </ul>
+            <div class="d-flex align-items-center justify-content-center">
+                <a href="#" class="btn btn-primary me-2">เข้าสู่ระบบ</a>
+                <a href="#" class="btn btn-outline-primary">สมัครสมาชิก</a>
+            </div>
+        </div>
+
+    </div>
+</nav>
